705ed7a16bd66c49acbd36db53df2fbc47e32531
Tryd0g0lik <work80@mail.ru>
2025-01-23 22:41:56 +0700

    [cloud_register]: Now recieved registration (front -> to back). But, I can not see an email massage from a refferal link

--- src/components/RegistrationPage/index.tsx
+++ src/components/RegistrationPage/index.tsx
@@ -51,7 +51,7 @@ export function RegistrationFormFC(props: HeaderForPages): JSX.Element {
               d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
           </svg>
           <input type="text" className="grow" placeholder="First name" name="first_name" />
-        </label> */}
+        </label>
         <label className="input input-bordered flex items-center gap-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -62,7 +62,7 @@ export function RegistrationFormFC(props: HeaderForPages): JSX.Element {
               d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
           </svg>
           <input type="text" className="grow" placeholder="Last login" name="last_login" />
-        </label>
+        </label> */}
         <label className="input input-bordered flex items-center gap-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
