5dd7420f49da1aa14930b95105cb384a6dfca30f
Tryd0g0lik <work80@mail.ru>
2025-01-30 22:28:49 +0700

    [cloud_activ]:  Code pages little was cleaned

--- src/components/LoginLogout/handlers/handlerOfProfileActivation.ts
+++ src/components/LoginLogout/handlers/handlerOfProfileActivation.ts
@@ -2,10 +2,8 @@
  * src\components\LoginLogout\handlers\handlerOfProfileActivation.ts
  */
 import { CookieUser } from "@Services/cookieServices";
-import Encrypto from "@Services/encrypts";
 import { Loginout } from "src/interfaces";
 import { changeDOM, buttonLoginLogout, fetches } from "@Services/scripts";
-import { result } from "lodash";
 /**
  * This function has two  entry-points.\
  *
@@ -34,10 +32,8 @@ const handlerLogin = (e?: React.MouseEvent | React.KeyboardEvent) => (key: strin
   ) {
     (e as React.MouseEvent).preventDefault();
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const regexUsername = /^[a-zA-Zа-яА-ЯёЁ\s]+$/;
     // Условие если был клик
     const formHtml = (e.target as HTMLFormElement).form;
-    // .parentElement as HTMLLabelElement).parentElement as HTMLFormElement);
 
     const map = new Map();
     for (let i = 0; i < formHtml.length; i++) {
@@ -72,10 +68,6 @@ const handlerLogin = (e?: React.MouseEvent | React.KeyboardEvent) => (key: strin
     // Условие если был клик
     data = JSON.stringify({ is_active: false });
   }
-  // else if (location.pathname.includes("users/login/")) {
-  //   (e as React.MouseEvent).preventDefault();
-  //   data = JSON.stringify({ is_active: true });
-  // }
   if ((data).length > 3) {
     const task0 = () => new Promise(resolve => resolve(fetches(data)
         .then(response => {
@@ -117,34 +109,3 @@ const handlerLogin = (e?: React.MouseEvent | React.KeyboardEvent) => (key: strin
 }
 export default handlerLogin;
 
-
-/**
- * We can get the number (index) of user from db.\
- * When the 'is_session' is 'true', from template string the '/user_session_\d+/' receive \
- * the user ID. It number (type string) and saving in the localstorage.
- * @param key string. It is key name the 'is_session' from a cookie.
- * @returns
- */
-// async function receivingDataOfFirstLogin(key: string) {
-//   // GET of key name from cookie
-//   const cookieUser = new CookieUser();
-//   cookieUser.sessionId = key;
-//   let result = cookieUser.getOneCookie();
-//   if (!result) {
-//     return false;
-//   }
-//   if ('true'.includes(result.toLowerCase())) {
-//     let cookie = (document.cookie).match(/user_session_\d+/) as Array<string> | null | string;
-//     // GET if user's number id from the key's name.
-//     cookie = cookie ? ((cookie as Array<string>).pop() as string).split("_").pop() as string : null;
-//     if (!result) {
-//       return false
-//     }
-//     const encrypto = new Encrypto(cookie as string);
-//     // Save the user's id
-//     localStorage.setItem("session", encrypto.encryptData());
-
-//   } else {
-//     null
-//   }
-// }
