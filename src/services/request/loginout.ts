1f417f88716b5fe02280454e6343190818821474
Tryd0g0lik <work80@mail.ru>
2025-02-06 03:13:16 +0700

    [profile]: Received the DECRYPT data

--- src/services/request/loginout.ts
+++ src/services/request/loginout.ts
@@ -3,7 +3,7 @@
 
 import { CookieUser } from "@Services/cookieServices";
 import { HttpMethods, UserAPI } from "@Interfaces";
-import { AES } from "crypto-ts";
+import { AES, mode, pad, enc } from "crypto-ts";
 /** ---- PATCH Method ----
 
  * @returns
@@ -12,11 +12,11 @@ export async function fetchLoginOut(prop: string) {
 
   let REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL as string;
   const cookie = new CookieUser();
-  const indexOfCookie = cookie.getOneCookie("index");
+  let indexOfCookie = cookie.getOneCookie("index");
   let url: string | URL = `${REACT_APP_SERVER_URL}${UserAPI.PATCH}${indexOfCookie}/`;
   let response: Response | undefined = undefined;
   if (!indexOfCookie) {
-    const secret_key = process.env.SECRET_KEY || "null";
+    const secret_key = process.env.REACT_APP_SECRET_KEY || "null";
     // Bufer bytes is a string, so we need to convert it to a string
     const buffer = new Int16Array(128 - secret_key.length);
     const bufferKey = new Uint32Array(buffer);
@@ -24,16 +24,23 @@ export async function fetchLoginOut(prop: string) {
       bufferKey[index] = secret_key.charCodeAt(byte)
     });
 
-    // const emailEncode: string = JSON.stringify({ data: CryptoJS.AES.encrypt(JSON.parse(prop)["email"], secret_key).toString() });
-    // const emailEncode = CryptoJS.AES.encrypt((JSON.parse(prop)["email"]), secret_key).toString()
-    // const emailEncode = AES.encrypt((JSON.parse(prop)["email"]), bufferKey).toString()
+
     const textEncoder = new TextEncoder();
-    const secret_key_b = textEncoder.encode(JSON.parse(prop)["email"]).toString();
+    const numbStr = Array.from(textEncoder.encode(secret_key)).join('');
+    // const key = enc.Utf8.parse(numbStr);
+    // const iv = enc.Utf8.parse(numbStr);
+    const key = enc.Utf8.parse(numbStr.slice(0, 32));
+    const iv = enc.Utf8.parse(numbStr.slice(0, 16));
 
 
-    const emailEncode = AES.encrypt(secret_key_b, secret_key).toString()
+    const encrypt = AES.encrypt(JSON.parse(prop)["email"], key, {
+      "iv": iv,
+      "mode": mode.CBC,
+      "padding": pad.PKCS7
+    }).toString()
     // response = await fetch(new URL(`${UserAPI.GETofAPI}${emailEncode.replace(":", "__null__")}/`, `${REACT_APP_SERVER_URL}`), {
-    url = new URL(`${UserAPI.CHOICE}name/${emailEncode.replace("/", "__null__")}/`, `${REACT_APP_SERVER_URL}`) as URL
+    url = new URL(`${UserAPI.CHOICE}name/`, `${REACT_APP_SERVER_URL}`) as URL
+    url.searchParams.set('data', encrypt);
     response = await fetch(url, {
       method: HttpMethods.GET,
       headers: {
@@ -44,7 +51,12 @@ export async function fetchLoginOut(prop: string) {
     if (!response.ok) {
       null
     }
+    const result = await response.json();
+    indexOfCookie = result['data'] as string;
+    url = `${REACT_APP_SERVER_URL}${UserAPI.PATCH}${indexOfCookie}/`;
   }
+
+
   // first is the 'GET' method for get a 'csrftoken'
   response = await fetch(`${REACT_APP_SERVER_URL}${UserAPI.BASIS}`)
   if (!response.ok) {
