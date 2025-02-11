399d83ea8026e281ec386b3d3d24386857a38a86
Tryd0g0lik <work80@mail.ru>
2025-02-11 18:44:33 +0700

    [profiltfile]: README was updated

--- src/components/Files/index.tsx
+++ src/components/Files/index.tsx
@@ -5,5 +5,68 @@ export function FilesdFC(maintitle: Maintitle ): JSX.Element{
   
   return(<>
     <NavbarTopFC {...maintitle} />
+    <section className="cloud-files ">
+      <div className="overflow-x-auto w-[100%] flex justify-center">
+        <table className="table-zebra  table-pin-rows w-[100%] max-w-screen-lg table-lg">
+          {/* head */}
+          <thead>
+            <tr >
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Num.</th>
+              <th>Name-file</th>
+              <th>Size</th>
+              <th>Date</th>
+              <th>Pablic-ref</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr className="hover">
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+              <td>Brown</td>
+            </tr>
+            {/* row 2 */}
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th >2</th>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support Technician</td>
+              <td>Purple</td>
+              <td>Brown</td>
+            </tr>
+
+            {/* row 3 */}
+            <tr className="hover">
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>3</th>
+              <td>Brice Swyre</td>
+              <td>Tax Accountant</td>
+              <td>Red</td>
+              <td>Brown</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
   </>);
 }
