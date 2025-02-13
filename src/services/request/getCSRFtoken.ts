8d9dd542d3b2c5142d7f14ac5f814337058a6bee
Tryd0g0lik <work80@mail.ru>
2025-02-13 13:34:32 +0700

    [profilefile]: he function fetchCSRF was added   to the services\request\getCSRFtoken.ts

---
+++ src/services/request/getCSRFtoken.ts
@@ -0,0 +1,26 @@
+/**
+ * src\services\request\getCSRFtoken.ts
+ */
+import { CookieUser } from "@Services/cookieServices";
+/**
+ * Get token "csrftoken". After the reseving the token,\
+ * it self save in cookie to the 10 secons. After that, the token will be deleted.
+ * @param url: string | URL - url of the server
+ * @returns object of type '{"csrftoken": string }'.
+ */
+export async function fetchCSRF(url: string | URL) {
+
+
+  const response = await fetch(url);
+  if (!response.ok && response.status !== 200) {
+    throw new Error(`[handlerFormFiles.ts::handlerFormFile]: Mistake => HTTP error! status: ${response.status}`);
+  }
+  const result = await response.json();
+
+  const cookie = new CookieUser()
+  cookie.sessionId = "csrftoken";
+  cookie.liveTimer = 10;
+  cookie.setCookie(result["csrftoken"] || "");
+  return result;
+
+}
